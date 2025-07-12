9569c84bf35c44e159422cca049a64e1a4a7cd7b
HMUNACHI <ndubuakuhenry@gmail.com>
2025-07-11 21:49:22 -0700

    Update .gitignore to include .kotlin directory; remove obsolete Kotlin compiler session file; enhance build-kotlin.sh script with additional command for running the example module.

--- scripts/build-kotlin.sh
+++ scripts/build-kotlin.sh
@@ -3,3 +3,4 @@
 # ./gradlew :library:iosX64MainKlibrary --quiet && echo "iOS library built successfully" && ls -la library/build/classes/kotlin/iosX64/main/
 # ./gradlew publishToMavenLocal
 # ls -la ~/.m2/repository/com/cactus/cactus/0.2.0/
+# ./gradlew :example:run
